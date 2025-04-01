caecff646f85e810a52f405531979600c359e860
Kieran Manning <mail@kmanning.ie>
2025-04-01 11:05:17 +0100

    chore: continuing with tutorial

--- tests/polls/test_views.py
+++ tests/polls/test_views.py
@@ -1,10 +1,30 @@
-import pytest
-
 from django.urls import reverse
+import pytest
 
 
 @pytest.mark.django_db
 def test_index_view(client):
     url = reverse("index")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_results_view(client):
+    url = reverse("results", kwargs={"question_id": 1})
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_detail_view(client):
+    url = reverse("detail", kwargs={"question_id": 1})
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_vote_view(client):
+    url = reverse("vote", kwargs={"question_id": 1})
     response = client.get(url)
     assert response.status_code == 200
